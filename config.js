924bd55296ac49355015d3f0f2afbf92c5d46e06
Dehkoyot <rokaf3@gmail.com>
2018-02-23 09:15:23 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   twitter: {
-    consumer_key: 'MY_CONSUMER_KEY',
-    consumer_secret: 'MY_CONSUMER_SECRET',
-    access_token_key: 'MY_ACCESS_TOKEN_KEY',
-    access_token_secret: 'MY_ACCESS_TOKEN_SECRET'
+    consumer_key: 'z2HOvZh9QGDB7N6N4MZidoVWo',
+    consumer_secret: 'dwzxAjy9wuE2mdUTzyJpSVgQli74beFoImZxBeAmN3YFPvdA3e',
+    access_token_key: '3091475697-rpBlanFGBm7EdYULntTLUecNEHwo1YMbLHCRO8O',
+    access_token_secret: 'zCKQQ1bwoBLFu6u50H8SYoUFexr8bLPmVimdzqrqyITJT'
   }
 }
